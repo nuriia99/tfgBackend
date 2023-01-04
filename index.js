543a761bc57365a4fbcefc458ef4e12a74f71437
nuria <nurianton1999@gmail.com>
2023-01-04 19:59:53 +0100

    add netlify cors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ export const app = express()
 dotenv.config()
 
 app.use(cors({
-  origin: ['http://localhost:3001', 'https://tfg-sistema-sanitario-upc.onrender.com']
+  origin: ['http://localhost:3001', 'https://tfg-sistema-sanitario-upc.onrender.com', 'https://tfg-sistema-sanitario-upc.netlify.app']
 })) // Use this after the variable declaration
 
 export const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
