7a14a093d4e5e4ddf831fdfe7068fd6965e5da10
nuria <nurianton1999@gmail.com>
2023-01-06 14:14:50 +0100

    improve rend patient

--- routes/schedules.js
+++ routes/schedules.js
@@ -4,13 +4,13 @@ import { verifyUser } from '../middleware/verifyUser.js'
 const router = express.Router()
 
 router.get('/getSchedule/:id', verifyUser, getSchedule)
+router.get('/getAppointments/:idPatient', verifyUser, getAppointments)
 router.get('/getSchedules', verifyUser, getSchedules)
 router.post('/createAppointment', verifyUser, createAppointment)
 router.patch('/updateAppointment/:id', verifyUser, updateAppointment)
 router.delete('/deleteAppointment/:id', verifyUser, deleteAppointment)
 router.delete('/deleteUrgAppointment/:id', verifyUser, deleteUrgAppointment)
 
-router.get('/getAppointments/:idPatient', getAppointments)
 router.post('/createUrgAppointment', createUrgAppointment)
 router.post('/createSchedule', createSchedule)
 
